63cb75f7e950f768be38dc980542dfdee4c3d30b
Jose Felix <jfelix000@west-mec.org>
2019-02-05 18:11:25 -0700

    Intro to Java
    Today we learned very minor things like how to comment 1 line vs many and how to use \n to have a break in the live code. We also checked out the developer tools in chrome and completed the first section to the active engagement sheet.

---
+++ tny_script.js
@@ -0,0 +1,18 @@
+//'use strict';
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Tutorial Case\\\
+   Countdown Clock
+         Author: Jose Felix
+         Date:   2.5.19 
+*/
+//book
+/*
+chik fil a
+The Blind Side
+*/
+//this is to come up at the beginning of the page (called an alert dialogue box).
+// \n is for a line break in code
+//window.alert('Welcome to \n Tulsa \n :o'); 
+
